f3bee4fd68db217e36342bc8fd32fa6f9dfd7d1a
flyzero <flyzeronew@gmail.com>
2024-01-30 01:46:14 +0800

    fix:開發推

--- comps/index/MainVision.js
+++ comps/index/MainVision.js
@@ -7,37 +7,40 @@ function MainVision(props) {
     const settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        自動播放速度: 3000  
     };  
     return (
             <div className="mainVision">
                 <Slider {...settings}>                
                     <div className="box">
-                        <img src="images/esg01.jpg" alt="img" width={1029} height={579}/>  
+                        <div className="img">
+                            <img src="images/esg01.jpg" alt="img" width={1029} height={579}/> 
+                        </div> 
                         <div className="txtBox">
                             <div className="title">台灣剩食危機</div>
                             <div className="txt">每人每天浪費一個便當！</div>
                             <div className="object">
                                 <div className="point"></div>
                                 <div className="arraw">
-                                <img src="images/icon_arraw02.svg" alt="arraw" width={50} height={50}/>
+                                    <img src="images/icon_arraw02.svg" alt="arraw" width={50} height={50}/>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="box">
-                        <img src="images/esg01.jpg" alt="img" width={1029} height={579}/>  
+                        <div className="img">
+                            <img src="images/esg04.jpg" alt="img" width={1029} height={579}/> 
+                        </div> 
                         <div className="txtBox">
                         <div className="title">台灣剩食危機</div>
                         <div className="txt">每人每天浪費一個便當！</div>
                         <div className="object">
                             <div className="point"></div>
                             <div className="arraw">
-                            <img src="images/icon_arraw02.svg" alt="arraw" width={50} height={50}/>
+                                <img src="images/icon_arraw02.svg" alt="arraw" width={50} height={50}/>
                             </div>
                         </div>
                         </div>
