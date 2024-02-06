86e1868dcc730bc7a022497612e7e33febb5b0bf
flyzero <flyzeronew@gmail.com>
2024-02-06 17:15:11 +0800

    加入共好空版

---
+++ pages/partner.js
@@ -0,0 +1,45 @@
+
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import Header from '../comps/Header'
+import Footer from '../comps/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+export default function Partner(props) {
+    console.log(props);
+// 頁面識別
+const thisPage='partner';
+
+return (
+<div id='wrapper' className={inter.className}> 
+    <Head>
+    <title>{"esg | 共好夥伴"}</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta name="Keywords" content="esg,esg2,esg3" />
+    <meta name="description" content="esg description" />        
+    </Head>
+    <Header thisPage={thisPage} menuData={props.menu}/>
+    <main>
+        
+    </main>
+    <div className="footerLine">
+        <div className="box"></div>
+    </div>
+    <Footer />
+</div>
+);
+}
+
+export async function getServerSideProps() {
+
+    const menuUrl = new URL('/api/menu', process.env.APP_URL);
+    const menuRes = await fetch(menuUrl);
+    const menu = await menuRes.json();
+
+    
+    return {
+        props: {
+            menu,focus
+        },
+    };
+}
