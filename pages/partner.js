fef1c2ace32e512e3cd638a3c15d9943572e55c0
JillTsai1218 <jilljane32@gmail.com>
2024-02-15 18:08:49 +0800

    feat: 共好夥伴列表頁 電腦版完成

--- pages/partner.js
+++ pages/partner.js
@@ -1,15 +1,51 @@
 
+
+import { useState ,useEffect } from 'react'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Header from '../comps/Header'
 import Footer from '../comps/Footer'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function Partner(props) {
     console.log(props);
 // 頁面識別
 const thisPage='partner';
+const [imgHover, setImgHover] = useState(null);
+const [bgSize, setBgSize] = useState();
+const [hoverBgSize, setHoverBgSize] = useState();
+    //圖片網址切換
+    const imgUrlChang = (url) => {
+        const originalUrl = "https://staging-esg-statics.s3.ap-northeast-1.amazonaws.com";
+        const imgUrl = process.env.IMG_URL;   
+        const newUrl = url;
+        const updateUrl = newUrl.replace(originalUrl, imgUrl);
+        return updateUrl;
+    };
+    const imgMouseOver = (e) => {
+        const isLargeScreen = window.innerWidth > 767;
+        setHoverBgSize(isLargeScreen ? 120 : 280);
+        setImgHover(e);
+    };
 
+    const imgMouseOut = (e) => {
+        setImgHover(null);
+    };
+    // resize 監聽事件
+    useEffect(() => { 
+        const handleResize = (e) => {
+            const newSize = window.innerWidth > 767 ? 100 : 250;
+            setBgSize(newSize);
+            setHoverBgSize(newSize);
+            setImgHover(null);
+        };  
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); 
 return (
     <div id='wrapper' className={inter.className}> 
         <Head>
@@ -20,7 +56,157 @@ return (
         </Head>
         <Header thisPage={thisPage} menuData={props.menu}/>
         <main>
-            
+            <div className="partnerPage">
+                <div className="sharedBanner">
+                    <div className="mask"></div>
+                    <div className="box">
+                        <div className="title">2023 共好夥伴</div>
+                        <div className="txt">
+                            <p>城市街頭盛開綠意，企業在環保徽章下環繞，共同編織著永續的未來。讓城市與企業一同前行，共創可持續的明日。</p>
+                            <div className="line"></div>
+                        </div>
+                    </div>
+                </div>   
+                <div className='submenu'>
+                    <a href="##">全部</a>
+                    <a href="##">永續企業</a>
+                    <a href="##">永續城市</a>
+                    <a href="##">團體與個人</a>
+                </div>  
+                <div className="list">
+                    <ul>
+                        <li key={1} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 1 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(1)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                        <li  key={2} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 2 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(2)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                        <li key={3} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 3 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(3)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        <span>追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設</span>
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li key={4} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 4 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(4)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                        <li  key={5} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 5 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(5)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                        <li key={6} style={{ 
+                                        background: `url("/images/partner-bg01.jpg") no-repeat center center`,
+                                        backgroundSize: 6 === imgHover ? `${hoverBgSize}%` : `${bgSize}%`,
+                                        transition: 'background-size 0.3s',
+                                    }} onMouseOver={() => imgMouseOver(6)} onMouseOut={imgMouseOut}>
+                            <a href='##'>
+                                <div className="profileCard">
+                                    <div className="profileImg">
+                                        <div className="img">
+                                            <Image src="/images/partner01.jpg" alt="img" width={50} height={50}/> 
+                                        </div>
+                                    </div>
+                                    <div className='name'>
+                                        台灣雀巢 Nestlé Taiwan
+                                    </div>
+                                    <div className='txt'>
+                                        <span>追求的目標是透過巧妙的包裝設計、創新材質、更完備的回收基礎建設</span>
+                                    </div>
+                                </div> 
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>       
+            </div>
         </main>
         <div className="footerLine">
             <div className="box"></div>
