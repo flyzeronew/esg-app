efccd0cd52659e1e63be6257a7838b79291b01b7
flyzero <flyzeronew@gmail.com>
2024-02-26 11:38:25 +0800

    回復舊有

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,7 @@ export default function Document() {
       <Head>
         <script
             dangerouslySetInnerHTML={{
-              __html: `
-                var process.env.GTM_ID;
+              __html: `           
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
