90c4f9370f14c1ece7d06ee84ddfa132b8989dd6
flyzero <flyzeronew@gmail.com>
2024-02-26 11:54:45 +0800

    fix:宣告方法調整

--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ export default function Document() {
         <NextScript />
         <noscript>
             <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
