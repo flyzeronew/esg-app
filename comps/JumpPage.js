e939672b833d3b431fcb2b40d6b4b4cb0338ce4d
flyzeronew <flyzeronew@gmail.com>
2024-03-12 18:24:28 +0800

    fix:自製跳頁

--- comps/JumpPage.js
+++ comps/JumpPage.js
@@ -5,7 +5,10 @@ function JumpList(props) {
     const router = useRouter();
     const { page = 1 } = router.query;
     const appUrl = process.env.APP_URL;
-    const uri =props.uri;
+    const uri = props.uri;
+    const pageCount = props.pageCount;
+    console.log(pageCount);
+    
    //插入跳頁按鈕顯示按鈕數量
     const jumpBtns = [];
     for (let i = 1; i <= 3; i++) {
@@ -18,21 +21,53 @@ function JumpList(props) {
             </div>
             <div className={`first ${Number(page)==1 ? 'hide':''}`}>
                 <a href={`${appUrl}${uri}?page=1`}>1</a>
-                <span>....</span>
+                <span>..</span>
             </div>
             <div className='box'>
+                {/* 倒數第1-2頁補頁數 */}
+                { page == pageCount ?
+                    <>
+                        <a href={`${appUrl}${uri}?page=${pageCount-2}`}>{pageCount-2}</a>
+                        <a href={`${appUrl}${uri}?page=${pageCount-1}`}>{pageCount-1}</a>
+                    </>
+                :''}
+                { page == pageCount-1 ?
+                    <>
+                        <a href={`${appUrl}${uri}?page=${pageCount-2}`}>{pageCount-2}</a>
+                    </>
+                :''}
+                {/* 倒數第1-2頁補頁數 ed*/}
+
                 {jumpBtns.map((item, index) => (
-                    <a 
-                        key={index} 
-                        className={item==1 ? 'act':''} 
-                        href={item > 1 ? `${appUrl}${uri}?page=${Number(page)+index}`:''}
-                    >
-                        {Number(page)+index}
-                    </a>
+                    <React.Fragment key={index}>                        
+                        { Number(page)+index <= pageCount?                        
+                            <a 
+                                className={item==1 ? 'act':''}
+                                href={item > 1 ? `${appUrl}${uri}?page=${Number(page)+index}`:''}
+                            >
+                                {Number(page)+index}
+                            </a>                        
+                            :''                        
+                        }
+                    </React.Fragment>
                 ))}
             </div>
+
+            {
+                page < pageCount-2 ? 
+                    <div className='last'>
+                        <span>..</span>
+                        <a href={`${appUrl}${uri}?page=${pageCount}`}>{pageCount}</a>                
+                    </div>
+                : ''
+            }
             <div className='btn'>
-                <a href={`${appUrl}${uri}?page=${Number(page)+1}`} >下一頁</a>
+                {   
+                    Number(page)+1 <= pageCount ? 
+                    <a href={`${appUrl}${uri}?page=${Number(page)+1}`} >下一頁</a>                    
+                    :
+                    <span className='lastTxt'>最後一頁</span>
+                }                
             </div>
         </div>
     )
