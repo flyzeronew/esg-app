3f74db739fcca1af2ebf80c231b04eaaa17c1d56
flyzeronew <flyzeronew@gmail.com>
2024-03-13 18:18:07 +0800

    fix:開發更新

--- pages/tips/[genres]/[page].js
+++ pages/tips/[genres]/[page].js
@@ -7,6 +7,17 @@ import Footer from '../../../comps/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function page(props) {
+    const [selectedOptions, setSelectedOptions] = useState([]);
+
+    const handleCheckboxChange = (e) => {
+        const value = e.target.value;
+        if (selectedOptions.includes(value)) {
+          setSelectedOptions(selectedOptions.filter(option => option !== value));
+        } else {
+          setSelectedOptions([...selectedOptions, value]);
+        }
+    };
+
     const appUrl = process.env.APP_URL;
     // 頁面識別
     const thisPage='tips';    
@@ -23,11 +34,77 @@ export default function page(props) {
             <div className="tipsDetailPage">
                 <div className="contentBox">
                     <div className="imgBox">
-                        <div className="frameBox">
-                            {/* <Image src="/images/tips01.jpg" alt="img" width={800} height={800}/>   */}
+                        <div className="box">
+                            <Image src="/images/tips01.jpg" alt="img" width={800} height={800}/>
                         </div>
                     </div>
+                    <div className="txtBox">
+                        <div className="question">
+                            <div className="tag">食</div>
+                            <div className="title">吃完的免洗紙餐盒，需洗完再回收嗎？</div>
+                            
+                            <div className="checkbox">
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        value="option1"
+                                        checked={selectedOptions.includes('option1')}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span>當然要洗啊～</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        value="option2"
+                                        checked={selectedOptions.includes('option2')}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span>我都直接丟</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        value="option3"
+                                        checked={selectedOptions.includes('option3')}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span>大概擦一下而已</span>
+                                </label>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        value="option4"
+                                        checked={selectedOptions.includes('option4')}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span>忘記了</span>
+                                </label>
+                            </div>
 
+                            <div className="btn">
+                                <span>送出答案</span>
+                                <div className="arraw tagFoodColor">
+                                    <Image src={`${appUrl}/images/icon_arraw07.svg`} alt="arraw" width={30} height={30}/>
+                                </div>
+                            </div>                   
+                        </div>
+
+                        <div className="answer">
+                            <div className="title">
+                                <Image src={`${appUrl}/images/smirking-face.png`} alt="arraw" width={50} height={50}/> 你答對了嗎？
+                            </div>
+                            <div className="answerTxtBox">
+                                <div className="answerTitle">免洗餐盒該當垃圾直接丟棄，還是洗淨回收？</div>
+                                <p>記者徐葳倫：「不說還真不知道，其實這個可分解的吸管，來自台灣的公司，連它(吸管)的包裝袋，都能夠分解，包含亞馬遜、蘋果、好市多和星巴克都是主要客戶，甚至在疫情期間，台灣口罩國家隊，所送往國外的口罩包裝袋，也是這家公司的產品。」</p>
+                            </div>
+                            <div className="answerTxtBox">
+                                <div className="answerTitle">免洗餐盒要丟一般垃圾還是資源回收？</div>
+                                <p>記者徐葳倫：「不說還真不知道，其實這個可分解的吸管，來自台灣的公司，連它(吸管)的包裝袋，都能夠分解，包含亞馬遜、蘋果、好市多和星巴克都是主要客戶，甚至在疫情期間，台灣口罩國家隊，所送往國外的口罩包裝袋，也是這家公司的產品。」</p>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
                 <div className="contentMore">                    
@@ -40,7 +117,7 @@ export default function page(props) {
                                         <div className="img">
                                             <Image src={`${appUrl}/images/tips01.jpg`} alt="img" width={300} height={300}/>
                                         </div>
-                                        <div className="tag">食</div>
+                                        <div className="tag tagFoodColor">食</div>
                                         <div className="txtBox">
                                             <div className='rounded'>
                                                 <Image src={`${appUrl}/images/rounded-05.svg`} alt="rounded" width={50} height={50}/>
@@ -61,7 +138,7 @@ export default function page(props) {
                                         <div className="img">
                                             <Image src={`${appUrl}/images/tips01.jpg`} alt="img" width={300} height={300}/>
                                         </div>
-                                        <div className="tag">食</div>
+                                        <div className="tag tagFoodColor">食</div>
                                         <div className="txtBox">
                                             <div className='rounded'>
                                                 <Image src={`${appUrl}/images/rounded-05.svg`} alt="rounded" width={50} height={50}/>
@@ -82,7 +159,7 @@ export default function page(props) {
                                         <div className="img">
                                             <Image src={`${appUrl}/images/tips01.jpg`} alt="img" width={300} height={300}/>
                                         </div>
-                                        <div className="tag">食</div>
+                                        <div className="tag tagFoodColor">食</div>
                                         <div className="txtBox">
                                             <div className='rounded'>
                                                 <Image src={`${appUrl}/images/rounded-05.svg`} alt="rounded" width={50} height={50}/>
