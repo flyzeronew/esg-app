8dee59f7a5e8104507cbb4183d4f5f2f7976746a
JillTsai1218 <jilljane32@gmail.com>
2024-03-14 10:30:52 +0800

    fix: 標題文字修正

--- pages/tips.js
+++ pages/tips.js
@@ -70,7 +70,7 @@ return (
                     <div className="sharedBanner">
                         <div className="mask"></div>
                         <div className="box">
-                            <div className="title">2024 共好夥伴</div>
+                            <div className="title">永續生活小撇步</div>
                             <div className="txt">
                                 <p>踏上永續生活的路上，小小的舉措，我們珍愛這片土地，永續生活於其中。</p>
                                 <div className="line"></div>
