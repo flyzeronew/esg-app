05bd95090be4e94a3fb900bb3ba2264a67de561c
flyzero <flyzeronew@gmail.com>
2024-03-18 21:57:39 +0800

    fix:補:key = {index}

--- comps/tips/List.js
+++ comps/tips/List.js
@@ -13,26 +13,26 @@ function tipsList(props) {
     return (
             <div className="list">
                 <ul>
-                    {listData.length > 0?
-                    listData.map((item, index) => (
-                        <li>
-                        <a href={item.url}>
-                            <div className={`genres ${tipsGenresArr[item.tip_genre.id-1]}`}>{item.tip_genre.name}</div>
-                            <div className="img">
-                                <Image src={item.img} alt="img" width={360} height={360}/>
-                            </div>
-                            <div className="detail">
-                            <div className='upImg'>
-                                <Image src="images/Rectangle-grey.svg" alt="img" width={20} height={20}/>
-                            </div>
-                                <div className="txt">{item.title}</div>
-                            <div className='rightImg'>
-                                <Image src="images/Rectangle-grey.svg" alt="img" width={20} height={20}/>
-                            </div>
-                            </div>
-                        </a>
-                    </li>
-                    ))  
+                    { listData.length > 0?
+                        listData.map((item, index) => (
+                            <li key = {index}>
+                                <a href={item.url}>
+                                    <div className={`genres ${tipsGenresArr[item.tip_genre.id-1]}`}>{item.tip_genre.name}</div>
+                                    <div className="img">
+                                        <Image src={item.img} alt="img" width={360} height={360}/>
+                                    </div>
+                                    <div className="detail">
+                                    <div className='upImg'>
+                                        <Image src="images/Rectangle-grey.svg" alt="img" width={20} height={20}/>
+                                    </div>
+                                        <div className="txt">{item.title}</div>
+                                    <div className='rightImg'>
+                                        <Image src="images/Rectangle-grey.svg" alt="img" width={20} height={20}/>
+                                    </div>
+                                    </div>
+                                </a>
+                            </li>
+                        ))  
                     :''}
                 </ul>
             </div>   
