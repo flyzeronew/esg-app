8bc3bcb7f5dc22bc7cd3e5d049a9b4fcdeca570a
flyzeronew <flyzeronew@gmail.com>
2024-03-19 18:38:56 +0800

    fix:修改程式

--- pages/view/[genres].js
+++ pages/view/[genres].js
@@ -16,7 +16,7 @@ export default function Genres(props) {
     // 頁面識別
     const thisPage='view';
     // 計算文章數量轉頁面數
-    const articleCount = props.articlesData.article_count-1;
+    const articleCount = props.articlesData.article_count;
     const articleMath = Math.floor(articleCount/12);
     const pageCount = articleCount % 12 != 0 ? articleMath + 1 : articleMath;
     // 計算文章數量轉頁面數 ed
@@ -30,7 +30,6 @@ export default function Genres(props) {
     const genreId = genreData ? genreData.id :'';
     const genreName = genreData ? genreData.name :'';
     const genreDescription =genreData ? genreData.description :'';
-console.log(pageCount);
     useEffect(() => {
         if (props.page > pageCount) {
             router.push('/404');
