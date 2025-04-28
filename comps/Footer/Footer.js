15a8860c31c29a14edcd45577f4270c60cee368c
flyzero <flyzero@tvbs.com.tw>
2025-04-28 10:37:55 +0800

    新增footer置底

--- comps/Footer/Footer.js
+++ comps/Footer/Footer.js
@@ -1,8 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Footer.module.css';
 
 function Footer(props) {
+  const [isFixed, setIsFixed] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      const wrapper = document.getElementById('wrapper');
+      const wrapperHeight = wrapper.offsetHeight;
+      const windowHeight = window.innerHeight;
+      setIsFixed(wrapperHeight < windowHeight);
+    };
+    handleResize(); 
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  console.log(isFixed);
   return (
-    <footer className={styles.footer}>
+    <footer 
+      className={`${styles.footer} ${isFixed ? styles.fixed : ''}`}
+    >
       © TVBS Media Inc. All Rights Reserved <br />
       聯利媒體股份有限公司
     </footer>
