612d4954d3e512edd0c01b41b2ee09252c9f3f54
flyzero <flyzeronew@tvbs.com.tw>
2025-09-30 13:57:31 +0800

    修改

--- app/robots.js
+++ app/robots.js
@@ -19,7 +19,8 @@ export default function robots() {
             rules: [
                 {
                     userAgent: ['*'],
-                    allow: ['/'],
+                    Disallow:['/'], 
+                    allow: ['/view'],
                 },
             ]
         }
